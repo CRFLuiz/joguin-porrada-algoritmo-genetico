1c005383341b3408989702bbdf6c5017422a1464
CRFLuiz <lcmartinscjr@gmail.com>
2021-03-01 21:41:39 -0300

    sync remote

---
+++ public/src/js/microfunctions/attacks.js
@@ -0,0 +1,5 @@
+// function attack(){
+// 	obj = {damage: 5, energy: -2, mana: 0, name: "KICK 1", enabled: true}; //{damage: 0, energy: 10, mana: 5, name: "HEALTHY A", enabled: true};
+
+// 	obj.
+// }
